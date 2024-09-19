fa80bbdb7cae33a584695e59460591f210c4a095
Andrew2797 <andrijgolub27@gmail.com>
2024-09-19 17:56:06 +0300

    idk

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+app = Flask(name)
+
 
 @app.get("/")
 def index():
-    return render_template("index.html", title="The Pizza Patch")
+    return render_template("index.html", title="Моя супер піцерія")
 
-@app.get("/menu")
+
+@app.get("/menu-ababagalamaga/")
 def menu():
     pizzas = [
-        {
-            "name": "mozzarella",
-            "price": "200 UAH",
-            "ingredients": "flour, eggs, sugar, salt, water, oil, yeast, Fresh or low-moisture mozzarella, Extra virgin olive oil, Salt & pepper, Fresh basil, garlic, chili flakes"
-        },
-        {
-            "name": "pepperoni",
-            "price": "250 UAH",
-            "ingredients": "flour, eggs, sugar, salt, water, oil, yeast, tomato sauce, pepperoni, cheese mozzarella, chili pepper"
-        },
-        {
-            "name": "four cheese",
-            "price": "300 UAH",
-            "ingredients": "flour, eggs, sugar, salt, water, oil, yeast, cheese mozzarella, cheese parmesan, cheese dor bleu, cheese cheddar"
-        }
+        {"name": "pepperoni", "price": "250 UAH", "ingredients": "flour, eggs, sugar, salt, water, oil, yeast, tomato sauce, pepperoni, cheese mozzarella, chili pepper"},
+        {"name": "pepperoni", "price": "290 UAH", "ingredients": "flour, eggs, sugar, salt, water, oil, yeast, tomato sauce, pepperoni, cheese mozzarella, chili pepper"},
+        {"name": "mozzarella", "price": "200 UAH", "ingredients": "flour, eggs, sugar, salt, water, oil, yeast, Fresh or low-moisture mozzarella, Extra virgin olive oil, Salt & pepper, Fresh basil, garlic, chili flakes"},
+        {"name": "four cheese", "price": "300 UAH", "ingredients": "flour, eggs, sugar, salt, water, oil, yeast, cheese mozzarella, cheese parmesan, cheese dor bleu, cheese cheddar"}
     ]
     context = {
         "pizzas": pizzas,
-        "title": "Mega menu"
+        "title": "Мега меню"
     }
     return render_template("menu.html", **context)
 
-if __name__ == "__main__":
+
+if name == "main":
     app.run(debug=True)
